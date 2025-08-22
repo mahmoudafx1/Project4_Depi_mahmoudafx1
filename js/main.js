63c280f88248da249486c7959117a4135f27b56a
mahmoudafx1 <148425386+mahmoudafx1@users.noreply.github.com>
2025-08-22 20:03:18 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+function convertDollar() {
+  var x = document.getElementById("dollar").value.trim();
+  var result = document.getElementById("result");
+
+  var num = Number(x);
+
+  if (x === "") {
+    result.innerHTML = "Enter a number, please";
+  } else if (num < 0) {
+    result.innerHTML = "Enter a positive number greater than 0";
+  } else if (num == 0) {
+    result.innerHTML = "Enter number rather than 0";
+  } else if (num > 0) {
+    var converted = num * 50;
+    result.innerHTML = num + "$ = " + converted + " L.E";
+  } else {
+    result.innerHTML = "Not a number, please enter a number";
+  }
+
+  return false;
+}
